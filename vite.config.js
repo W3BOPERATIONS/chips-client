bfd48b2f23a774ea4763016e556f5386ae8b52eb
Alis Patel <alispatel123098@gmail.com>
2025-09-26 12:23:05 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://server-api-one-psi.vercel.app/",
         changeOrigin: true,
       },
     },
@@ -16,3 +16,6 @@ export default defineConfig({
     outDir: "dist",
   },
 })
+
+
+
